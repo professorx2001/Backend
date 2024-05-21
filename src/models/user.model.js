0bfb2014ec2b8d3b34d3c359acf614c8b51ab80e
Md Zaki Hussain <mdzakihusain@gmail.com>
2024-05-21 20:30:30 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -76,6 +76,7 @@ userSchema.methods.generateRefreshToken = function(){
     {
         expiresIn : process.env.REFRESH_TOKEN_EXPIRY
     }
+)
 }
 
 export const User = mongoose.model("User", userSchema)
